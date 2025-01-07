b1bcbd852327a90d7c1552683ea03a3a87a28f28
VictorLuu <victorluu99@gmail.com>
2025-01-07 22:58:11 +0700

    fix build

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,29 +17,29 @@ export const metadata: Metadata = {
   },
   description: siteConfig.description,
   icons: {
-    icon: siteConfig.icon,
-    shortcut: siteConfig.logo,
-    apple: siteConfig.logo
+    // icon: siteConfig.icon,
+    // shortcut: siteConfig.logo,
+    // apple: siteConfig.logo
   },
   twitter: {
     card: 'summary_large_image',
     title: siteConfig.title,
     description: siteConfig.description,
-    images: [siteConfig.landing]
+    // images: [siteConfig.landing]
   },
   openGraph: {
     type: 'website',
     url: siteConfig.url,
     title: siteConfig.title,
     description: siteConfig.description,
-    images: [
-      {
-        url: siteConfig.landing,
-        width: 1200,
-        height: 630,
-        alt: 'Landing Meta Image'
-      }
-    ]
+    // images: [
+    //   {
+    //     url: siteConfig.landing,
+    //     width: 1200,
+    //     height: 630,
+    //     alt: 'Landing Meta Image'
+    //   }
+    // ]
   },
   viewport: 'width=device-width, initial-scale=1.0',
   robots: 'index, follow'
